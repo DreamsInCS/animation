c8581009fc93a37b79f6cd1a855e463a5a3039b8
DreamsInCS <yoannierhermida@knights.ucf.edu>
2020-02-11 17:42:23 -0500

    App completed!

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   createState() => HomeState();
@@ -8,23 +9,47 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
+  Animation<double> boxAnimation;
   AnimationController catController;
+  AnimationController boxController;
 
   initState() {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this
     );
 
-    catAnimation = Tween(begin: 0.0, end: 100.0)
-      .animate(
-        CurvedAnimation(
-          parent: catController,
-          curve: Curves.easeIn
-        )
-      );
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this
+    );
+
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn
+      )
+    );
+
+    boxAnimation = Tween(begin: pi * 0.6, end: pi * 0.65).animate(
+      CurvedAnimation(
+        parent: boxController,
+        curve: Curves.easeInOut
+      )
+    );
+
+    boxController.forward();
+
+    boxController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      }
+      else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
   }
 
   Widget build(context) {
@@ -34,37 +59,41 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       ),
       body: GestureDetector(
         child: Center(
-          child: Stack(children: <Widget>[
-            buildCatAnimation(),
-            buildBox()
-            ]
+          child: Stack(
+            overflow: Overflow.visible,
+              children: <Widget>[
+                buildCatAnimation(),
+                buildBox(),
+                buildLeftFlap(),
+                buildRightFlap()
+              ]
           ),
         ),
         onTap: onTap
       )
     );
-  }
-
+  }  
+ 
   onTap() {
     if (catController.status == AnimationStatus.dismissed) {
+      boxController.stop();
       catController.forward();
     }
     else if (catController.status == AnimationStatus.completed) {
+      boxController.forward();
       catController.reverse();
     }
-  }
-
-  Widget buildAnimation() {
-
   }
 
   Widget buildCatAnimation() {
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (context, child) {   // Child argument helps Flutter not rebuild the entire cat a bunch (60times/sec)
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value)
+          top: catAnimation.value,
+          left: 0.0,
+          right: 0.0
         );
       },
       child: Cat()      // We'll only alter one quality of the animation upon rebuild, saving time!
@@ -76,6 +105,48 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       height: 200.0,
       width: 200.0,
       color: Colors.brown[400]
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned ( 
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown[400]
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topRight,
+            angle: -1 * boxAnimation.value
+          );
+        }
+      )
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned ( 
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown[400]
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value
+          );
+        }
+      )
     );
   }
 }
